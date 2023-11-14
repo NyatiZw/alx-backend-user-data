f0e6f297b416831a2a6cede97ce5a0201963575c
NyatiZw <appsmaid19@gmail.com>
2023-11-14 10:40:26 +0000

    Log formatter

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,32 +1,35 @@
 #!/usr/bin/env python3
 """Implementing a log filter that will obfuscate PII fields"""
 
+
+import logging
 import re
 from typing import List
 
 
-def filter_datum(
-        fields: List[str],
-        redaction: str,
-        message: str,
-        separator: str) -> str:
+class RedactingFormatter(logging.Formatter):
+    """Redacting Formatter class
     """
-    Function that logs obfuscated data
 
-    Args:
-        fields: list of strings
-        redaction: string of obfuscated fields
-        message: string representing the log line
-        separator: string representing char separating fields
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
 
-    Returns:
-        log message
-    """
-    return re.sub(
-            f'({separator.join(fields)}){separator}(.*?){separator}',
-            f'\\1{redaction}{separator}',
-            message
-        )
+    def __init__(self, fields: List[str]):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+
+    def format(self, record: logging.LogRecord) -> str:
+        def filter_datum(fields, redaction, message, separator):
+            return re.sub(
+                    f'({separator.join(fields)}){separator}(.*?){separator}',
+                    f'\\1{redaction}{separator}',
+                    message
+                )
+
+        record.msg = filter_datum(self.fields, self.REDACTION, record.msg, self.SEPARATOR)
+        return super(RedactingFormatter, self).format(record)
 
 
 if __name__ == "__main__":
