6a17c2d308fb79aec028f1cffed08014e2878270
NyatiZw <appsmaid19@gmail.com>
2023-11-14 10:49:28 +0000

    Log formatter

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
-"""Implementing a log filter that will obfuscate PII fields"""
+"""
+Implementing a log filter that will obfuscate PII fields
+"""
 
 
 import logging
@@ -8,7 +10,17 @@ from typing import List
 
 
 class RedactingFormatter(logging.Formatter):
-    """Redacting Formatter class
+    """
+    Redacting Formatter class
+
+    Attributes:
+    - REDACTION (str): The string used for redaction
+    - FORMAT (str): The log message format
+    - SEPARATOR (str): The field separator in log messages
+
+    Methods:
+    - __init__(self, fields: List[str]): Constructor method
+    -format(self, record: logging.LogRecord) -> str: Formats records
     """
 
     REDACTION = "***"
@@ -16,10 +28,25 @@ class RedactingFormatter(logging.Formatter):
     SEPARATOR = ";"
 
     def __init__(self, fields: List[str]):
+        """
+        Initialize the RedactingFormatter
+
+        Args:
+        - fields (List[str]): A list of strings representing fields
+        """
         super(RedactingFormatter, self).__init__(self.FORMAT)
         self.fields = fields
 
     def format(self, record: logging.LogRecord) -> str:
+        """
+        Format log records with redacted fields
+
+        Args:
+        - record (logging.LogRecord): The log record to format
+
+        Returns:
+        - str: The formatted log message
+        """
         def filter_datum(fields, redaction, message, separator):
             return re.sub(
                     f'({separator.join(fields)}){separator}(.*?){separator}',
